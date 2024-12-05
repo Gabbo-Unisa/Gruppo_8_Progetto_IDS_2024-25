9895514a335f3647fffcc3fc3338eab479bdafcd
Carmine Terracciano <carminet2004@gmail.com>
2024-12-05 22:10:40 +0100

    Corretto gestione package

--- src/main/java/rubrica/Models/ApplicazioneProgetto.java
+++ src/main/java/rubrica/Models/ApplicazioneProgetto.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ApplicazioneProgetto extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ApplicazioneProgetto.class.getResource("/rubrica/Views/elencoContatti.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicazioneProgetto.class.getResource("/rubrica/Views/ElencoContattiView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Rubrica Telefonica");
         stage.setScene(scene);
