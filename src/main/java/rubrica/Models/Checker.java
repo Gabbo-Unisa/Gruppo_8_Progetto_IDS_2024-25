0aec40f679e5b9505399a38a1f22f7982837905d
Gabriele Pannuto <gabbi97@live.it>
2024-12-06 22:30:15 +0100

    Documentazione Doxygen classi Checker e FileManager

--- src/main/java/rubrica/Models/Checker.java
+++ src/main/java/rubrica/Models/Checker.java
@@ -2,7 +2,19 @@ package rubrica.Models;
 
 public class Checker {
 
-    public boolean validaContatto(){
+
+    /**
+     * @brief Effettua il controllo sulla validità dei campi "telefono" ed "email" di un contatto
+     *
+     * @param[in] c Il contatto da validare
+     *
+     * @pre Il contatto deve avere almeno nome e/o cognome settati.
+     *
+     * @post Viene stabilito se i campi "telefono" ed "email" del contatto sono legittimi.
+     *
+     * @return Ritorna un valore true se il contatto è valido, altrimenti false.
+     */
+    public boolean validaContatto(Contatto c){
         return false;
     }
 }
