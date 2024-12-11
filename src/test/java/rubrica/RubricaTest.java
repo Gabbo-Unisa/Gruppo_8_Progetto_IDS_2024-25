ce7e5be01f42475522e7a8e6d23cd43df6e0c019
Pasquale Piccolo <p.piccolo5@studenti.unisa.it>
2024-12-11 18:26:56 +0100

    creazione classe RubricaTest.java

---
+++ src/test/java/rubrica/RubricaTest.java
@@ -0,0 +1,46 @@
+package rubrica;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import rubrica.Models.Checker;
+import rubrica.Models.Rubrica;
+
+class RubricaTest {
+
+    private Rubrica rubrica;
+    private Checker checker;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testGetContatti() {
+
+    }
+
+    @Test
+    void testAggiungiContatto() {
+    }
+
+    @Test
+    void testModificaContatto() {
+    }
+
+    @Test
+    void testEliminaContatto() {
+    }
+
+    @Test
+    void testRicercaContatti() {
+    }
+
+    @Test
+    void testGetContattiPreferiti() {
+    }
+}
