b69b3c8ee613954502d439b5ba34d76cb2d61ce2
Ferdinando <ferdypaparo03@gmail.com>
2024-12-11 18:43:19 +0100

    creazione classe ContattoTest.java

---
+++ src/test/java/rubrica/ContattoTest.java
@@ -0,0 +1,76 @@
+package rubrica;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ContattoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setNome() {
+    }
+
+    @Test
+    void setCognome() {
+    }
+
+    @Test
+    void setNumeriTelefono() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void setNota() {
+    }
+
+    @Test
+    void setPreferito() {
+    }
+
+    @Test
+    void getNome() {
+    }
+
+    @Test
+    void getCognome() {
+    }
+
+    @Test
+    void getNumeriTelefono() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getDataCreazione() {
+    }
+
+    @Test
+    void getNota() {
+    }
+
+    @Test
+    void getIsPreferito() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
